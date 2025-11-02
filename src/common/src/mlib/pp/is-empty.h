5625fea78835324fd5a2d58609319c3793c7fad2
vector-of-bool <vectorofbool@gmail.com>
2025-11-02 14:19:50 -0700

    Fix: Wrong macro def placement

--- src/common/src/mlib/pp/is-empty.h
+++ src/common/src/mlib/pp/is-empty.h
@@ -140,5 +140,19 @@
  */
 #define _mlibCommaIfParens(...) ,
 
+/*-
+ * This helper macro expands to its 64th argument, no matter how many
+ * arguments it is given. You must pass at least 64 arguments to this macro
+ */
+#define _mlibPick64th(...) _mlibPick64th_impl(__VA_ARGS__)
+#define _mlibPick64th_impl( \
+                 _0,  _1,  _2,  _3,  _4,  _5,  _6,  _7,  _8,  _9, \
+                _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, \
+                _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, \
+                _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, \
+                _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, \
+                _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, \
+                _60, _61, _62, _63, ...) \
+    _63
 
 #endif // MLIB_PP_IS_EMPTY_H_INCLUDED
