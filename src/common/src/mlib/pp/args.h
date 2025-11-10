4f23f714258fb9250f6e77fc30baf600dda18613
vector-of-bool <vectorofbool@gmail.com>
2025-11-10 11:43:44 -0700

    Simpler ARG_COUNT

--- src/common/src/mlib/pp/args.h
+++ src/common/src/mlib/pp/args.h
@@ -41,30 +41,27 @@
  * should be wrapped in parentheses for such function-like macros.
  */
 #define MLIB_ARG_COUNT(...) \
-   _mlibPick64th( \
-        /*-
-         * Expand the argument list. For every top-level comma, shifts the
-         * argument list over by 1.
-         */ \
-        __VA_ARGS__ \
-        /*-
-         * If given no arguments, don't insert the first comma, preventing us
-         * from incorrectly yielding `1` for zero arguments:
-         */ \
-        MLIB_OPT_COMMA(__VA_ARGS__) \
-        /*-
-         * Argument counts, up to 63. For each top-level comma in __VA_ARGS__,
-         * these are shifted along, with `0` in the 64th position to start.
-         * To support more than 15 arguments, more integers must be added,
-         * and `_mlibPick64th` needs to be extended to a higher number.
-         */ \
-        63, 62, 61, 60, 59, 58, 57, 56, 55, 54, \
-        53, 52, 51, 50, 49, 48, 47, 46, 45, 44, \
-        43, 42, 41, 40, 39, 38, 37, 36, 35, 34, \
-        33, 32, 31, 30, 29, 28, 27, 26, 25, 24, \
-        23, 22, 21, 20, 19, 18, 17, 16, 15, 14, \
-        13, 12, 11, 10,  9,  8,  7,  6,  5,  4, \
-         3,  2,  1,  0, ~)
+    MLIB_IF_ELSE(MLIB_IS_EMPTY(__VA_ARGS__)) \
+        (0) \
+        (_mlibPick64th( \
+            /*-
+            * Expand the argument list. For every top-level comma, shifts the
+            * argument list over by 1.
+            */ \
+            __VA_ARGS__, \
+            /*-
+            * Argument counts, up to 63. For each top-level comma in __VA_ARGS__,
+            * these are shifted along, with `0` in the 64th position to start.
+            * To support more than 15 arguments, more integers must be added,
+            * and `_mlibPick64th` needs to be extended to a higher number.
+            */ \
+            63, 62, 61, 60, 59, 58, 57, 56, 55, 54, \
+            53, 52, 51, 50, 49, 48, 47, 46, 45, 44, \
+            43, 42, 41, 40, 39, 38, 37, 36, 35, 34, \
+            33, 32, 31, 30, 29, 28, 27, 26, 25, 24, \
+            23, 22, 21, 20, 19, 18, 17, 16, 15, 14, \
+            13, 12, 11, 10,  9,  8,  7,  6,  5,  4, \
+            3,  2,  1,  0, ~))
 
 /**
  * @brief Expand to a different function-like macro invocation based on
